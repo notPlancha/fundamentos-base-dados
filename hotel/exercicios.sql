a4da0099319dcf9ca2978af34042e0e19ff4cd46
notPlancha <andre.plancha@hotmail.com>
2022-10-07 14:31:24 +0100

    hotel exercicios feitos 1 - 12, 15, 16

--- hotel/exercicios.sql
+++ hotel/exercicios.sql
@@ -1,13 +1,81 @@
 use aluno_105289_hoteldb;
 
+-- 1
+-- Listar o nome de todos os clientes por ordem alfabética decrescente
+select Nome_Cliente from cliente order by Nome_Cliente DESC ;
+
+-- 2
+-- Listar o número dos quartos do hotel cuja sigla é RM
+select q.Numero_Quarto from hotel h
+    join quarto q on h.Sigla_Hotel = q.Sigla_Hotel
+where h.Sigla_Hotel = 'RM';
+
+-- 3
+-- Quantos hotéis existem na nossa base de dados?
+select count(*) as '# Hoteis' from hotel;
+
+-- 4
+-- Quantos quartos têmo hotel Majestic (sigla MJ)? E quantas camas têm? (apenas um comando)
+select count(Numero_Quarto) as '# Quartos', sum(Numero_Camas) as '# Camas' from hotel h
+    join quarto q on h.Sigla_Hotel = q.Sigla_Hotel
+where h.Sigla_Hotel = 'MJ';
+
+-- 5
+-- Listar o nome de todos os clientes individuais
+select Nome_Cliente from individual i
+    left join cliente c on c.Numero_Cliente = i.Numero_Cliente;
+
+-- 6
+-- Listar o nome de todos os clientes organizações que trabalham no ramo Alimentar
+select Nome_Cliente from organizacao o
+    left join cliente c on c.Numero_Cliente = o.Numero_Cliente
+where Ramo_Actividade = 'Alimentar';
+
+-- 7
+-- Quais os nomes dos clientes individuais que não têm BI
+select Nome_Cliente from individual i
+    left join cliente c on c.Numero_Cliente = i.Numero_Cliente
+where i.BI is null;
+
+-- 8
+-- Quais as designações dos hotéis que têm reservas em que pelo menos um quarto está afecto a mais do que duas pessoas?
+select h.Designacao from reserva_quartos rq
+    left JOIN hotel h on h.Sigla_Hotel = rq.Sigla_Hotel
+where Numero_Pessoas > 2;
+
+-- 9
+-- Quais a designação dos hotéis que têm reservas para o mês de Setembro e/ou Agosto de 2012? (basta que um dos dias da
+-- reserva calhe entre 1 Agosto e 30 de setembro para que sejam seleccionados)
+select distinct h.Designacao from reserva r
+    left join reserva_quartos rq on r.Numero_Reserva = rq.Numero_Reserva
+    left join hotel h on rq.Sigla_Hotel = h.Sigla_Hotel
+where
+    (r.Dia_Entrada between '2012-8-1' and '2012-10-1')
+ or (r.Dia_Saida between '2012-8-1' and '2012-10-1');
+
+-- 10
+-- Quais os números de reserva efectuados pela ONU e pela Ana?
+select r.Numero_Reserva
+from reserva r
+    left join cliente c on r.Numero_Cliente = c.Numero_Cliente
+where Nome_Cliente in ('ONU', 'Ana');
+
 -- 11
---
+-- Liste os nomes dos hotéis onde o cliente ISCTE já efectuou reservas
 select
     hotel.Designacao, count(DISTINCT f.Numero_Factura) as c
 from hotel
     left join reserva_quartos as rq on hotel.Sigla_Hotel = rq.Sigla_Hotel
     left join factura as f on rq.Numero_Reserva = f.Numero_Reserva
 GROUP BY hotel.Designacao order by c DESC;
+
+-- 12
+-- Listar, para cada designação do hotel, a quantidade de facturas
+select h.Designacao, count(distinct f.Numero_Factura)
+from factura f
+    left join reserva_quartos rq on f.Numero_Reserva = rq.Numero_Reserva
+    right join hotel h on rq.Sigla_Hotel = h.Sigla_Hotel
+group by h.Sigla_Hotel;
 -- 15
 -- Para cada nome de uma organização, indicar o total de reservas existentes nessa organização
 select
